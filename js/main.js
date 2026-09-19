f2ae54fa656e62d2baed6d10d61805e66af0b077
Kinza Rehman <kinzarehman38@gmail.com>
2026-09-19 19:49:38 -0400

    Adding HTML

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
-// alert('works')
+//alert('works')
+
+/*Goal: Create a two player Tic-Tac-Toe game. 
+The users should be able to click to place 
+their X or O and if they win the program
+should mention their win in the DOM. 
+Please make the game as OOP as possible.
+*/
+
+
+/* 
+step 1: Create the two players
+step 2: User interaction (OOP)
+    What happens if X or O wins?
+        - If X wins then HTTML playerXscore updates
+        -If O wins then playerOscore updates
+    What happens if no one wins? 
+        -the points get added to html id  Tie 
+step 3: Which array patters determine the win?
+    If either user gets three in a row:
+        [0 1 2]
+        [3 4 5]
+        [6 7 8]
+    Win patterns vertical, horizontal and X
+    total win patterns within the array is 8? 
+Step 4: if the person wins it should mention thier win in the DOM(document Object model):
+    ex: document.write(" play ${playerXwin} won!") idk
+Step 5: if theres one player then it shoul duse an API and have a computer play against the user
+if i complete the basics on time then integrate step 5 else just simple
+*/
+
+
+
+
